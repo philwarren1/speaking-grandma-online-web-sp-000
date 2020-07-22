7d509efb5cf0360e25edbefefc28ff1a7eb1f4fa
Philip Warren <philwarren1@gmail.com>
2020-07-22 21:01:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,37 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+   if "#{phrase}" == "I LOVE YOU GRANDMA!"
+   return "I LOVE YOU TOO PUMPKIN!"
+   elsif "#{phrase}".upcase
+     "NO, NOT SINCE 1938!"
+   else
+     "HUH?! SPEAK UP, SONNY!"
+  end
+end    
+
+def greeting(name)
+  puts "Hello, #{name}"
+end
+
+
+def greeting(name = "Ruby programmer")
+  puts "Hello, #{name}"
+end
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+greeting("Sophie")
+
+dog = "thirsty"
+ 
+if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+else
+  puts "Reading newspaper."
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
